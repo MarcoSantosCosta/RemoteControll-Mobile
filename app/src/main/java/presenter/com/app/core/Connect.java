b62f48c983be58ace4428056aa2757cebf4670af
Marco Costa <marcotuliocosta@live.com>
2018-01-29 02:08:50 -0200

    Sistema de recebimento de ação adicionado

--- app/src/main/java/presenter/com/app/core/Connect.java
+++ app/src/main/java/presenter/com/app/core/Connect.java
@@ -4,40 +4,76 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import presenter.com.app.buffers.ActionBuffer;
 import presenter.com.app.buffers.MessageBuffer;
+import presenter.com.messages.Action;
 import presenter.com.messages.Message;
 
 public class Connect extends AsyncTask<Void, Void, Void> {
 
+    private String ip;
+    private int port;
+    private ActionBuffer actionBuffer;
+    private MessageBuffer messageBuffer;
+    private Socket socket;
+
+    public Connect(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+        this.messageBuffer = MessageBuffer.getInstance();
+    }
+
+    public boolean connect() {
+        Socket client = null;
+        try {
+            client = new Socket(this.ip, this.port);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return client != null;
+    }
+
+    public void send(ObjectOutputStream output) throws IOException {
+        Message aux = messageBuffer.getMessage();
+        if (aux != null) {
+            output.writeObject(aux);
+            output.flush();
+            output.reset();
+        }
+    }
+
+    public void recive(ObjectInputStream input) throws IOException, ClassNotFoundException {
+        Object aux = input.readObject();
+        if (aux instanceof Action) {
+            Action act = (Action) aux;
+            actionBuffer.addAction(act);
+        } else {
+            Log.e("Error","Não reconhecido");
+        }
+
+    }
+
 
     @Override
     protected Void doInBackground(Void... voids) {
-        MessageBuffer buffer = MessageBuffer.getInstance();
         try {
-            Socket client = new Socket("192.168.0.101", 12345);
-            try {
-                ObjectOutputStream output = new ObjectOutputStream(client.getOutputStream());
-                long last = System.currentTimeMillis();
-                while (true) {
-                    long now = System.currentTimeMillis();
-                    if (now - last > 1000) {
-                        Message aux = buffer.getMessage();
-                        System.out.println(aux);
-                        if (aux != null) {
-                            output.writeObject(aux);
-                            output.flush();
-                            output.reset();
-                        }
-                        last = now;
-                    }
+            ObjectOutputStream output = new ObjectOutputStream(this.socket.getOutputStream());
+            ObjectInputStream input = new ObjectInputStream(this.socket.getInputStream());
+            long last = System.currentTimeMillis();
+            while (true) {
+                long now = System.currentTimeMillis();
+                if (now - last > 100) {
+                    send(output);
+                    recive(input);
+                    last = now;
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
         return null;
